896e0201d32057fb6c864550f337b38fd2285692
Nereida Rodríguez <nereida@LAPTOP-VAJBTHLP.localdomain>
2022-08-21 19:33:43 +0200

    added how to do a boolean props

--- modulo-3-react/modulo-3-leccion-6-refactorizar-ev-intermedia-modulo-1/src/components/Header.js
+++ modulo-3-react/modulo-3-leccion-6-refactorizar-ev-intermedia-modulo-1/src/components/Header.js
@@ -6,30 +6,8 @@ const Header = () => {
   return (
     <div>
       <header className="header">
-        {/*<Logo />*/}
-        <a href="https://adalab.es/">
-          <img
-            className="logo"
-            src="../images/adalab-logo-155x61.png"
-            alt="logo adalab"
-            title="Inicio"
-          />
-        </a>
-        {/*<Menu />*/}
-        <nav className="nav">
-          <ul className="ul">
-            <li className="blog">
-              <a className="link-nav" href="https://adalab.es/blog/">
-                blog
-              </a>
-            </li>
-            <li className="contacto">
-              <a className="link-nav" href="https://adalab.es/contacto/">
-                contacto
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <Logo />
+        <Menu />
       </header>
     </div>
   );
---
+++ modulo-3-react/modulo-3-leccion-6-refactorizar-ev-intermedia-modulo-1/src/components/Links.js
@@ -0,0 +1,23 @@
+const Links = (props) => {
+  const typeTarget = props.openInNewTab ? "_blank" : ""; //si es true ponme "_blank"
+  return (
+    <li className={props.text}>
+      <a
+        className="link-nav"
+        href="https://adalab.es/blog/"
+        target={typeTarget}
+      >
+        {props.text}
+      </a>
+    </li>
+  );
+};
+
+/*¿Qué es eso de typeTarget? Lo de poner una constante en una propiedad de una etiqueta HTML no es nuevo,
+recuerda que eso se hace cuando quieres variar una clase y cosas así, como hacíamos en el juego del ahorcado 
+del modulo 3 o en las paletas del módulo 2. En este caso, esa propiedad está regida por una condición,
+escrita en un ternario. Esa condición tiene, a su vez, la posibilidad de que una props (que es lo nuevo de
+esta lección, lo que luego se define en la madre), sea true (en cuyo caso se cambiará por "_blank"
+o que sea false (en cuyo caso se cambiará por ""). Así se hace una props BOOLEANA*/
+
+export default Links;
